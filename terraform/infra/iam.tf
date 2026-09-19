dbf71b07742fea41713786429705af5979725869
Avinash <avinash.ayyala@gmail.com>
2026-09-19 00:52:29 -0400

    simplify app dependencies and add AWS OIDC authentication

---
+++ terraform/infra/iam.tf
@@ -0,0 +1,77 @@
+# GitHub Actions OIDC identity provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+# IAM role that GitHub Actions is allowed to assume
+resource "aws_iam_role" "github_actions" {
+  name = "devops-app-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:avi6650/devops-app:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Permissions the GitHub Actions role gets after assuming it
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "devops-app-ecr-push"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+
+        Resource = aws_ecr_repository.devops_app.arn
+      }
+    ]
+  })
+}
