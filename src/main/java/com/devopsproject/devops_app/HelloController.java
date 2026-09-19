7e7192c87a6a62f70c0e941bd0df5169e5c7cdd5
Avinash <avinash.ayyala@gmail.com>
2026-09-19 17:48:30 -0400

    Add Kubernetes configuration and ALB ingress

--- src/main/java/com/devopsproject/devops_app/HelloController.java
+++ src/main/java/com/devopsproject/devops_app/HelloController.java
@@ -1,13 +1,17 @@
 package com.devopsproject.devops_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${APP_MESSAGE:Hello from DevOps App!}")
+    private String appMessage;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from DevOps App!";
+        return appMessage;
     }
 }
